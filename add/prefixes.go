c7afba52d68e973b5087b1a9e51c7d2d4a4044a3
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-16 07:39:52 -0400

    feat: added function to return the content with the prefixes
    * Added function to return the content with the prefixes. Forked from 'github.com/ralvarezdev/go-databases'

---
+++ add/prefixes.go
@@ -0,0 +1,18 @@
+package add
+
+import (
+	gostringsseparator "github.com/ralvarezdev/go-strings/separator"
+	"strings"
+)
+
+// Prefixes returns a string with the given content and prefixes
+func Prefixes(
+	content string,
+	separator gostringsseparator.Separator,
+	prefixes ...string,
+) string {
+	// Push content to the end of the prefixes string slice
+	prefixes = append(prefixes, content)
+
+	return strings.Join(prefixes, string(separator))
+}
