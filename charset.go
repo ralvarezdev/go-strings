7d5b9ec9a5a298eaf33e657fa57475f8966f3adf
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-11-10 16:20:15 -0400

    feat: added alphanumeric charset from github.com/ralvarezdev/go-crypto

---
+++ charset.go
@@ -0,0 +1,6 @@
+package gostrings
+
+const (
+	// AlphanumericCharset is the character set used to generate random UTF-8 safe strings
+	AlphanumericCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
